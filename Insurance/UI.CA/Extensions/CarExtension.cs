61de35fe4ac133197d906f26a341e9b6ec0cb703
Jonas Leijzen <jonasleijzen@gmail.com>
2022-03-28 22:11:39 +0200

    Small fixes

--- Insurance/UI.CA/Extensions/CarExtension.cs
+++ Insurance/UI.CA/Extensions/CarExtension.cs
@@ -8,7 +8,7 @@ namespace Insurance.UI.CA.Extensions
         internal static string GetInfo(this Car c)
         {
             string garage = c.Garage != null
-                ? string.Format(ViewLocalizationResources.Managed_By_Garage, c.Garage)
+                ? string.Format(ViewLocalizationResources.Managed_By_Garage, c.Garage.Name)
                 : string.Empty;
             return string.Format (ViewLocalizationResources.GetInfo_Car,
                 c.NumberPlate, c.Brand, c.Fuel.GetName (), garage, c.PurchasePrice);
